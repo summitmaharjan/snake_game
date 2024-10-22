6e6f9c4207d659a29d1467d543ed25be974bfd4b
summitmaharjan <smtmhrzn5@gmail.com>
2024-10-22 20:28:43 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+#     detect collision with food:::
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+#     detect collision with wall::
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
+
+
+#     detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+screen.exitonclick()
+
+# we can do this too without using loop:::
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+#
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
